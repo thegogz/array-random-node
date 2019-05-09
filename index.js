88d33eb221784d0e37c54a75c6bae90851abe2e7
Eoin O'Neill <thegogz@gmail.com>
2019-05-09 23:09:12 +0100

    Cleanup of modules

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-const _ = require('lodash');
+const ranArray = require("./lib/randomArray");
 
-var invalidNumbers = [0];
+var test = ranArray.generateRandomArray(10);
 
-var getRandomInt = function()
-{
-    let number = Math.floor(Math.random() * 10);
-    if(_.includes(invalidNumbers, number)){
-        console.log('there was an invalid char');
-        return getRandomInt();
-    }
-    return number;
-}
-
-var generateRandomArray = function(length)
-{
-    var numArray = [];
-
-    for (let i = 0; i < length; i++) {
-        numArray.push(getRandomInt());
-    }
-
-    return numArray;
-}
-
-var onlyOneNumberArray = function(length, number)
-{
-    let numArray = [];
-    for (let i = 0; i < length; i++) {
-        numArray.push(number);
-    }
-    return numArray;
-}
-
+console.log(test);
